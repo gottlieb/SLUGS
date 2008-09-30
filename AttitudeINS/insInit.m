b02383694c430a2824a9e658f0bc859e3c01a29e
Mariano Lizarraga <malife@gmail.com>
2008-09-30 11:15:19 -0700

    Initial steps for Elkaim-Demoz KFilter

--- AttitudeINS/insInit.m
+++ AttitudeINS/insInit.m
@@ -1,7 +1,7 @@
 Qk = diag([0.1 0.1 0.1 0.1 0.1 0.1 0.1]);
 Rk = diag([3 3 5]);
 
-%X0 = [quatnormalize(euler2quat(Euler_0)) 0.001 0.001 0.001];
+X0 = [quatnormalize(euler2quat(Euler_0)) 0.001 0.001 0.001];
 P0 = diag([.3 .3 .3 .3  0.75 0.75 0.75 ]);
 
 latUp = 100000;
