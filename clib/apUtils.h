d631017c6bd41f452a96f5cfb6e7de785f21aab1
Mariano Lizarraga <malife@gmail.com>
2008-11-02 11:46:18 -0800

    Correction of ATAN2 bug in Simulink. Attitude working on bench; ready for cart-test

---
+++ clib/apUtils.h
@@ -0,0 +1,15 @@
+#ifndef _APUTILS_H_
+#define _APUTILS_H_
+
+#ifdef __cplusplus
+       extern "C"{
+#endif
+#include <math.h>       	
+float myAtan2(float num, float denom);       	
+       	
+#ifdef __cplusplus
+      }
+#endif
+
+
+#endif /* _APUTILS_H_ */
