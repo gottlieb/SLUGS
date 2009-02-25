f9a0d13a7162b734f7327e9be4a22e428b826f22
Mariano Lizarraga <malife@gmail.com>
2009-02-24 16:39:38 -0800

    Lateral and Longitudinal channel implemented and tested for stability. Longitudinal channel tuned for simulator. Need to tune Lateral and implement Navigation

--- InnerOuterLoop/innerOuterInit.m
+++ InnerOuterLoop/innerOuterInit.m
@@ -11,3 +11,8 @@ drLimit = 10*pi/180;
 uHoldLimit = 0.85*dtLimit;          % Controls throttle
 heightErrorLimit = 15*pi/180;       % this loop generates theta_command
 pitchErrorLimit = 0.9*deLimit;      % controls elevator
+yawDamperLimit = 0.9*drLimit;       % controls rudder
+rollControlLimit = 0.9*daLimit;     % controls aileron
+
+%% Command Limits
+pitchCommandLimit = 15*pi/180;
