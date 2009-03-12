6eba542cc206aa0d05371d7e8d3fd1a216342163
Mariano Lizarraga <malife@gmail.com>
2009-03-11 23:05:31 -0700

    Minor modification. Added preturn to initial WP0 to WP1 nav

--- InnerOuterLoop/randomInit.m
+++ InnerOuterLoop/randomInit.m
@@ -24,28 +24,32 @@ wpCount = floor(getRanVal(7,4,10));
 wps = zeros(wpCount,3);
 
 % generate the next waypoint
-x_wp = randn*5000;
-y_wp = randn*5000;
+x_wp = randn*2000;
+y_wp = randn*2000;
 z_wp = getRanVal(750,550,1000);
 
 % add it to the array
 wps(1,:) = [x_wp, y_wp, z_wp];
 
 for ii=2:wpCount
-    reiterate = 1;    
-    while reiterate == 1
+    reiterate = 1; 
+    attempts = 0;
+    while (reiterate == 1) && (attempts < 2000)
+        reiterate = 0;
+        
         % generate the next waypoint
-        x_wp = randn*5000;
-        y_wp = randn*5000;
+        x_wp = randn*2000;
+        y_wp = randn*2000;
         z_wp = getRanVal(750,550,1000);
 
-        reiterate = 0;
         % check the separation between all WPs
         for mm = 1:(ii-1)
            if (norm(wps(mm,1:2)-wps(mm+1,1:2)) < 500)
                reiterate = 1;
            end
         end
+        attempts = attempts+1;
+        reiterate;
     end
 
     wps(ii,:) = [x_wp, y_wp, z_wp];
@@ -58,7 +62,9 @@ Ypoints = wps(:,2);
 Zpoints = wps(:,3);
 
 %% Random Wind
-windBase = getRanVal(12,4,15);
+windBase = getRanVal(4,4,7.5);
 windDirTurb = getRanVal(360,0,360);
 windDirHor = getRanVal(360,0,360);
+
+wps
 
