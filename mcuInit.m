0d7fb3e6661bf0db963be2ec8a6bc70d3f002a9b
Mariano Lizarraga <malife@gmail.com>
2009-03-17 17:22:10 -0700

    Started PWM Calibrations process

--- mcuInit.m
+++ mcuInit.m
@@ -1,6 +1,10 @@
+%% Gains
 % Global Sample Time
 T = 0.01;
 load gainsAndBiases
 gxGainrad = gxGain*pi/180;
 gyGainrad = gyGain*pi/180;
 gzGainrad = gzGain*pi/180;
+
+%% Run the Inner outer loop init
+run ./InnerOuterLoop/innerOuterInit.m
