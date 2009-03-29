8bd5ad2c11dceb7c895a85f3cd364323f8f72d0e
Mariano Lizarraga <malife@gmail.com>
2009-03-29 12:59:05 -0700

    Added initializer file for HIL simulation

---
+++ HILSim/hilInit.m
@@ -0,0 +1,28 @@
+%% Set up the values for the HIL Simulation
+HIL_K = 3;
+
+
+%% Run the standard setup files
+run ../InnerOuterLoop/innerOuterInit.m
+run ../InnerOuterLoop/Rascal_Var.m
+
+%% Set up the reverse lookup values for PWM to radians
+% Rudder
+P =  polyfit([rudMin rudMax],[-calVal calVal],1);
+mPWMdr = P(1);
+bPWMdr = P(2);
+
+% Aileron
+P =  polyfit([ailMin ailMax],[-calVal calVal],1);
+mPWMda = P(1);
+bPWMda = P(2);
+
+% Elevator
+P =  polyfit([eleMin eleMax],[-calVal calVal],1);
+mPWMde = P(1);
+bPWMde = P(2);
+
+% Throttle
+P =  polyfit([thrMin thrMax],[0 1],1);
+mPWMdt = P(1);
+bPWMdt = P(2);
