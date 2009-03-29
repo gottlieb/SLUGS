ec05eea2e55795ea7ddc841526236b43adfedaff
Mariano Lizarraga <malife@gmail.com>
2009-03-28 17:43:16 -0700

    Modified HIL simulink model to include the latest 6DOF model and the conversion from PWM to radians. Model ready for HIL tests

--- InnerOuterLoop/innerOuterInit.m
+++ InnerOuterLoop/innerOuterInit.m
@@ -24,11 +24,36 @@ R = single(200);
 L1_base = single(200);
 U_comm = single(25);
 
-%% Curve fitting for PWM in Actual Code
+%% Curve fitting for PWM . Conversion from Radians to PWM
 % Calibration value in radians
 calVal = 25*pi/180;
+
 % Rudder
-P =  polyfit([-calVal calVal],[4550 3300],1);
+rudMin = 4550;
+rudMax = 3300;
+P =  polyfit([-calVal calVal],[rudMin rudMax],1);
 mdr = P(1);
 bdr = P(2);
+
+% Aileron
+ailMin = 4550;
+ailMax = 3300;
+P =  polyfit([-calVal calVal],[ailMin ailMax],1);
+mda = P(1);
+bda = P(2);
+
+% Elevator
+eleMin = 4550;
+eleMax = 3300;
+P =  polyfit([-calVal calVal],[eleMin eleMax],1);
+mde = P(1);
+bde = P(2);
+
+
+%Throttle
+thrMin = 4550;
+thrMax = 3300;
+P =  polyfit([0 1],[thrMin thrMax],1);
+mdt = P(1);
+bdt = P(2);
 
