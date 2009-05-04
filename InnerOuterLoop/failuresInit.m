fe4053b6e15e12d4bb346c985b6efc699aa9ca18
Mariano Lizarraga <malife@gmail.com>
2009-05-04 15:24:50 -0700

    Trying a different psi_dot computation

--- InnerOuterLoop/failuresInit.m
+++ InnerOuterLoop/failuresInit.m
@@ -2,13 +2,13 @@
 
 % Activate Failures
 daL_fail = 0;
-daR_fail = 0;
+daR_fail = 1;
 dr_fail  = 0;
 
 % Simulation time for the failure (secs)
-tfailure = 50;
+tfailure = 30;
 
-da_failamp = 15;
+da_failamp = 20;
 da_failper = 1500;
 da_failbias = 0;
 
