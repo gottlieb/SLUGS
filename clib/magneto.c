45799f28ad20c25a7abfdf9d26839626684626d6
Mariano Lizarraga <malife@gmail.com>
2009-05-14 12:17:02 -0700

    Magnetometer Ready to be used in the AP

--- clib/magneto.c
+++ clib/magneto.c
@@ -1,12 +1,31 @@
 #include "magneto.h"
 
-// Current state Variable
-unsigned char i2c1State;
-unsigned char reg2Config;
-tShortToChar currentMag;	
-unsigned char byteRead, wordRead;
-unsigned char debugCount;
-tShortToChar  magReading[3];
+/*
+The endianess of the dsPIC was determined with an experiment as follows
+
+	placeTest.chData[0] = 1;
+	placeTest.chData[1] = 0;
+	
+	
+	placeTest2.chData[0] = 0;
+	placeTest2.chData[1] = 1;
+	
+	printToUart2("%u\t %u\n\r",placeTest.usData, placeTest2.usData);
+	The Above line produces:
+		1		256
+	therefore, chData[0]= LSB and chData[1]=MSB
+
+*/
+
+unsigned char i2c1State;		// State Machine State
+unsigned char reg2Config;		// Register to config
+
+// Declared global to avoid having them be static 
+// inside the interrupt
+tShortToChar currentMag;		// placeholder for current mag reading
+unsigned char byteRead, wordRead; // current byte and word read
+unsigned char byteCount;		// Count the number of bytes received
+
 
 void magDebugUartConfig(void){
 	// Configure and open the port;
@@ -44,8 +63,7 @@ void magDebugUartConfig(void){
 void magnetoInit (void){
 	magDebugUartConfig();
 	
-	printToUart2("Starting %s\n\r","sequence");
-	unsigned int i, j;
+	//printToUart2("Starting %s\n\r","sequence");
 	
 	
 	// Configure the I2C bus
@@ -65,138 +83,81 @@ void magnetoInit (void){
 	
 	// Initialize the state machine
 	i2c1State = CONFIG_IDLE;
-	reg2Config = 0x00;
+	// Select the register to configure
+	reg2Config = REGISTER_A;
 	
-	printToUart2("Starting %s\n\r","I2C");
-
-	// Put some huge delays to wait for Magnetometer power-up without the need of a timer
-	// 5 milliseconds are expected for power-up
-	for( i = 0; i < 15; i += 1 ){
-		for( j = 0; j < 32700; j += 1 )
-		{
-			Nop();
-		}
-	}
+	//printToUart2("Starting %s\n\r","I2C");
 	
 	// Change the mode to 50 Hz
-	
+	// ========================
 	// Start The Bus
 	i2c1Start();
 	// Wait for the bus stop
 	// this signal that the whole config went trhough
 	while(i2c1State != CONFIG_IDLE){
 		printToUart2("Out of Int: %d\n\r",i2c1State);
+		//byteRead++;
 	}
 	
-	printToUart2("Changed To %s\n\r","50 HZ");
+	//printToUart2("Changed To %s\n\r","50 HZ");
 
-	// Put some huge delays to wait for Magnetometer power-up without the need of a timer
-	// 5 milliseconds are expected for power-up
-	for( i = 0; i < 15; i += 1 ){
-		for( j = 0; j < 32700; j += 1 )
-		{
-			Nop();
-		}
-	}	
+	// Wait 5 mS
+	dummyDelay();
 	
-	// Change the gain
-	// Change the register to config
-	reg2Config = 0x01;
-	// Start The Bus
-	i2c1Start();
-	// Wait for the bus stop
-	// this signal that the whole config went trhough
-	while(i2c1State != CONFIG_IDLE){
-		printToUart2("Out of 2nd I: %d\n\r",i2c1State);
-	}
-
-	// Put some huge delays to wait for Magnetometer power-up without the need of a timer
-	// 5 milliseconds are expected for power-up
-	for( i = 0; i < 15; i += 1 ){
-		for( j = 0; j < 32700; j += 1 )
-		{
-			Nop();
-		}
-	}	
-	
-	printToUart2("Changed To %s\n\r","0.7 Gain");
-	
-
-
 	// Change the mode to continous
 	// Change the register to config
-	reg2Config = 0x02;
+	reg2Config = MODE_REGISTER;
 	// Start The Bus
 	i2c1Start();
 	// Wait for the bus stop
 	// this signal that the whole config went trhough
 	while(i2c1State != READ_IDLE){
 		printToUart2("Out of 3rd I: %d\n\r",i2c1State);
+		//byteRead++;
 	}
-
-	// Put some huge delays to wait for Magnetometer power-up without the need of a timer
-	// 5 milliseconds are expected for power-up
-	for( i = 0; i < 15; i += 1 ){
-		for( j = 0; j < 32700; j += 1 )
-		{
-			Nop();
-		}
-	}	
 	
-	printToUart2("Changed To %s\n\r","Contrinous");
+	//printToUart2("Changed To %s\n\r","Contrinous");
 
-
-
-
-
+	// Wait 5 mS
+	dummyDelay();
+	
+	// Initialize the variables
 	byteRead = 1;
 	wordRead = 0;
-	debugCount = 0;
+	byteCount = 0;
 }
 
 void startMagRead (void) {
 	i2c1State = READ_IDLE;
 	byteRead = 1;
 	wordRead = 0;
-	debugCount = 0;
+	byteCount = 0;
 	i2c1Start();
 	
 }
 
 void readMag (unsigned char idx, short val){
+	static tShortToChar magReading[3];
+	
 	magReading[idx].shData	= val;
 	
 	if (idx == 2){
-		/*rawControlData.magX.usData = magReading[0].usData;
-		rawControlData.magY.usData = magReading[1].usData;
-		rawControlData.magZ.usData = magReading[2].usData;*/
-		//printToUart2("==dc:%u\n\r", debugCount);
-		printToUart2("%d\t %d\t %d\n\r",magReading[0].shData, magReading[1].shData,magReading[2].shData);
+		rawControlData.magX.usData = (unsigned short) (magReading[0].shData+ 2048);
+		rawControlData.magY.usData = (unsigned short) (magReading[1].shData+ 2048);
+		rawControlData.magZ.usData = (unsigned short) (magReading[2].shData+ 2048);
+		
+	//	printToUart2("%d\t %d\t %d\n\r",magReading[0].shData, magReading[1].shData,magReading[2].shData);
 	}
 }
 
-void getMag (short * magVals){
-	//tUnsignedShortToChar placeTest, placeTest2;
+void getMag (unsigned short * magVals){
 	
-	magVals[0] =  magReading[0].shData;
-	magVals[1] =  magReading[1].shData;
-	magVals[2] =  magReading[2].shData;
+	magVals[0] =  rawControlData.magX.usData;
+	magVals[1] =  rawControlData.magY.usData;
+	magVals[2] =  rawControlData.magZ.usData;
 	//printToUart2("%u\t %u\t %u\n\r",magVals[0], magVals[1],magVals[2]);
 	
-	/*placeTest.chData[0] = 1;
-	placeTest.chData[1] = 0;
-	
-	
-	placeTest2.chData[0] = 0;
-	placeTest2.chData[1] = 1;
-		
-	
-	printToUart2("%u\t %u\n\r",placeTest.usData, placeTest2.usData);
-	The Above line produces:
-		1		256
-	therefore, chData[0]= LSB and chData[1]=MSB
-	*/
-	
+	// After reporting the data start the reading for the next cycle
 	startMagRead();
 }
 
@@ -221,20 +182,17 @@ void i2c1Write(unsigned char byte2write){
 	I2C1TRN = byte2write;
 }
 
+// Interrupt Service Routine (State Machine Implementation)
+// =========================
 
-
-void __attribute__((__interrupt__, no_auto_psv)) _MI2C1Interrupt(void){
-	//printToUart2("==== Stat: %d\n\r",i2c1State);
-	//printToUart2("at:%d rb:%d \n\r",I2C1STATbits.ACKSTAT, I2C1STATbits.RBF);
-	//printToUart2("br:%d wr:%d \n\r",byteRead, wordRead);
-		
+void __attribute__((__interrupt__, no_auto_psv)) _MI2C1Interrupt(void){		
 	switch (i2c1State) {
 		case CONFIG_START:
 			if (!I2C1CONbits.SEN){
 				// change the state
 				i2c1State = CONFIG_IDTX;
 				// Send the address to write
-				i2c1Write(0x3C);
+				i2c1Write(MAG_WRITE);
 			}
 		break;
 		case CONFIG_IDTX:
@@ -248,12 +206,12 @@ void __attribute__((__interrupt__, no_auto_psv)) _MI2C1Interrupt(void){
 		case CONFIG_REG_TX:
 			if (!I2C1STATbits.ACKSTAT){
 				i2c1State = CONFIG_VAL_TX;				
-				if (reg2Config==0){
+				if (reg2Config==REGISTER_A){
 					// send the actual configuration for 50Hz
-					i2c1Write(0x18);
+					i2c1Write(MODE_50_HZ);
 				} else {
 					// send the configuration for continuous sampling
-					i2c1Write(0x00);
+					i2c1Write(MODE_CONTINUOS);
 				}
 			}
 		break;
@@ -266,7 +224,7 @@ void __attribute__((__interrupt__, no_auto_psv)) _MI2C1Interrupt(void){
 		break;
 		case CONFIG_STOP:
 			if (!I2C1CONbits.PEN){
-				if (reg2Config!=0x02){
+				if (reg2Config!=MODE_REGISTER){
 					// Set the Config to idle, wait for next config
 					i2c1State = CONFIG_IDLE;
 				} else {
@@ -280,7 +238,7 @@ void __attribute__((__interrupt__, no_auto_psv)) _MI2C1Interrupt(void){
 				// change the state
 				i2c1State = READ_IDTX;
 				// Send the address to write
-				i2c1Write(0x3D);
+				i2c1Write(MAG_READ);
 			}		
 		break;
 		case READ_IDTX:
@@ -294,16 +252,16 @@ void __attribute__((__interrupt__, no_auto_psv)) _MI2C1Interrupt(void){
 		break;	
 		case READ_DATA:
 			if (I2C1STATbits.RBF){
-				//printToUart2("br:%d wr:%d dc:%d\n\r",byteRead, wordRead,debugCount);
-				debugCount++;
+				//printToUart2("br:%d wr:%d dc:%d\n\r",byteRead, wordRead,byteCount);
+				byteCount++;
 				// read the received Data
-				if (debugCount < 7) {
+				if (byteCount < 7) {
 					currentMag.chData[byteRead] = (unsigned char)I2C1RCV;
 					//printToUart2("byte:%d\n\r", I2C1RCV);
 				} else {
 					wordRead= (unsigned char)I2C1RCV;
 					byteRead = 1;
-					printToUart2("ignored:%d\n\r", I2C1RCV);
+					//printToUart2("ignored:%d\n\r", I2C1RCV);
 				}
 				
 				// if we just read a word (i.e. byteRead == 1)
@@ -318,7 +276,7 @@ void __attribute__((__interrupt__, no_auto_psv)) _MI2C1Interrupt(void){
 				byteRead = byteRead == 0? 1 : 0;
 				
 				//if (wordRead >= 3){
-				if (debugCount >= 7 ){
+				if (byteCount >= 7 ){
 					// Done Generate a NACK 
 					I2C1CONbits.ACKDT = 1;
 		            I2C1CONbits.ACKEN = 1;
@@ -329,7 +287,7 @@ void __attribute__((__interrupt__, no_auto_psv)) _MI2C1Interrupt(void){
 				}
 			} else if (!I2C1STATbits.ACKSTAT){
 				//if (wordRead < 3){
-				if (debugCount < 7){
+				if (byteCount < 7){
 					// Enable reading
 					I2C1CONbits.RCEN = 1;					
 				} else {
@@ -361,166 +319,17 @@ void printToUart2 (const char *fmt, ...){
 	putsUART2((unsigned int*)buf);
 }
 
-/*
-		case READ_START:
-			if (!I2C1CONbits.SEN){
-				// change the state
-				i2c1State = READ_IDTX;
-				// Send the address to write
-				i2c1Write(0x3D);
-			}		
-		break;
-		case READ_IDTX:
-			if (!I2C1STATbits.ACKSTAT){
-				// change the state
-				i2c1State = READ_RESTART;
-				// Generate a Restart Event
-				I2C1CONbits.RSEN = 1;
-			}		
-		break;
-		case READ_RESTART:
-			if (!I2C1CONbits.RSEN){
-				// change the state
-				i2c1State = READ_REGADD;
-				// Write the Register address
-				i2c1Write(0x03);
-			}		
-		break;	
-		case READ_REGADD:
-			if (!I2C1STATbits.ACKSTAT){
-				// change the state
-				i2c1State = READ_DATA;
-				// Enable reading
-				I2C1CONbits.RCEN = 1;
-			}		
-		break;	
-		case READ_DATA:
-			if (!I2C1STATbits.RBF){
-				// read the received Data
-				currentMag.chData[byteRead] = (unsigned char)I2C1RCV;
-				// if we just read a word (i.e. byteRead == 1)
-				if(byteRead == 1)
-				{
-					readMag(wordRead,currentMag.usData);
-					wordRead++;
-				}
-				
-				// flip the byte
-				byteRead = !byteRead;
-				
-				if (wordRead >= 3){
-					// change the state
-					i2c1State = READ_DONE;
-					i2c1Stop();					
-				} else {
-					// Enable reading
-					I2C1CONbits.RCEN = 1;					
-				}
-				
-			}		
-		break;	
-		case READ_STOP:
-			if (!I2C1CONbits.PEN){
-				// Ready to read next magnetometer data
-				i2c1State = READ_IDLE;				
-			}
-		break;	
-*/
-
-
-/*
-		case READ_START:
-			if (!I2C1CONbits.SEN){
-				// change the state
-				i2c1State = READ_IDTX;
-				// Send the address to write
-				i2c1Write(0x3D);
-			}		
-		break;
-		case READ_IDTX:
-			//if (!I2C1CONbits.RSEN){
-			if (!I2C1STATbits.ACKSTAT){
-				// change the state
-				i2c1State = READ_REGADD;
-				// Write the Register address
-				i2c1Write(0x02);
-			}		
-		break;	
-		case READ_REGADD:
-			if (!I2C1STATbits.ACKSTAT){
-				// change the state
-				i2c1State = READ_RESTART;
-				// Enable reading
-				I2C1CONbits.SEN =1;
-			}		
-		break;	
-		case READ_RESTART:
-			if (!I2C1CONbits.SEN){
-				// change the state
-				i2c1State = READ_IDADD2;
-				// Send the address to write
-				i2c1Write(0x3D);
-			}
-		break;
-		case READ_IDADD2:
-			if (!I2C1STATbits.ACKSTAT){
-				// change the state
-				i2c1State = READ_DATA;
-				// Enable reading
-				I2C1CONbits.RCEN = 1;
-			}		
-		break;	
-		case READ_DATA:
-			if (I2C1STATbits.RBF){
-				debugCount++;
-				// read the received Data
-				if (debugCount < 7) {
-					currentMag.chData[byteRead] = (unsigned char)I2C1RCV;
-					printToUart2("byte:%d", I2C1RCV);
-				} else {
-					wordRead= (unsigned char)I2C1RCV;
-					byteRead = 1;
-				}
-				
-				// if we just read a word (i.e. byteRead == 1)
-				if(byteRead == 0)
-				{
-					//printToUart2("%u\n\r",currentMag.usData);
-					readMag(wordRead,currentMag.shData);
-					wordRead++;
-				}
-				
-				// flip the byte
-				byteRead = byteRead == 0? 1 : 0;
-				
-				if (wordRead >= 3){
-				//if (debugCount < 7 ){
-					// Done Generate a NACK 
-					I2C1CONbits.ACKDT = 1;
-		            I2C1CONbits.ACKEN = 1;
-				} else {
-					// Generate the AKN
-					I2C1CONbits.ACKDT = 0;
-            		I2C1CONbits.ACKEN = 1;
-				}
-			} else if (!I2C1STATbits.ACKSTAT){
-				if (wordRead < 3){
-				//if (debugCount < 7){
-					// Enable reading
-					I2C1CONbits.RCEN = 1;					
-				} else {
-					// change the state
-					i2c1State = READ_DONE;
-					i2c1Stop();					
-				}	
-			}
-		break;	
-		case READ_STOP:
-			if (!I2C1CONbits.PEN){
-				// Ready to read next magnetometer data
-				i2c1State = READ_IDLE;				
-			}
-		break;	
-
-*/
+void dummyDelay (void) {
+	unsigned int i, j;
+	// Put some huge delays to wait for Magnetometer power-up 
+	// without the need of a timer
+	// 5 milliseconds are expected for power-up
+	// @ 40Mhz requires aprox 200,000 nops
+	for( i = 0; i < 7; i += 1 ){
+		for( j = 0; j < 32700; j += 1 )
+		{
+			Nop();
+		}
+	}
+}
 
