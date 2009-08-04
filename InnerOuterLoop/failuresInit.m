3deb0add609ff54f686372871ecd47697ca26f2b
Mariano Lizarraga <malife@gmail.com>
2009-08-04 09:25:03 -0700

    Added some plots to the parseTelemetry

--- InnerOuterLoop/failuresInit.m
+++ InnerOuterLoop/failuresInit.m
@@ -8,7 +8,7 @@ dr_fail  = 0;
 % Simulation time for the failure (secs)
 tfailure = 30;
 
-da_failamp = 15;
+da_failamp = 5;
 da_failper = 1500;
 da_failbias = 0; 
 
@@ -19,4 +19,4 @@ dr_failbias = 0;
 %% Initialize the L1
 L1OF = 1;
 L1CS =1;
-compensatorOn = 0;
+compensatorOn = 1;
