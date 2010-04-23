ded09263436eb2a07ea9cc89b479bbc82e07aa53
Mariano Lizarraga <malife@gmail.com>
2010-04-23 13:01:10 -0500

    Matched the Simulation and Flight files to be identical. Corrected minor bug in plotting the simulation results

--- InnerOuterLoop/plotResults/plotCirc.m
+++ InnerOuterLoop/plotResults/plotCirc.m
@@ -1,14 +1,14 @@
 cent = O.signals.values;
 Pchs = Pch.signals.values;
 PchBs = PchB.signals.values;
-
+Rval = R1.signals.values;
 
 tp = 0:0.1:2*pi;
 
 figure(figct)
 hold on;
 for ii = 4:1:length(cent)
-    plot(cent(ii,2)+R*cos(tp), cent(ii,1)+R*sin(tp),'k');
+    plot(cent(ii,2)+Rval(ii)*cos(tp), cent(ii,1)+Rval(ii)*sin(tp),'k');
     plot(cent(ii,2), cent(ii,1),'vk');
     
     plot(Pchs(ii,2),Pchs(ii,1), 'vr');
